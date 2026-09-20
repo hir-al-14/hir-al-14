56a546fb0281424e48bb712ad543f44905131b40
Hiral Arora <hiral.arora1406@gmail.com>
2026-09-20 00:03:47 -0700

    Add math import to generate_stats.py

--- scripts/generate_stats.py
+++ scripts/generate_stats.py
@@ -5,6 +5,7 @@ from datetime import datetime, timezone
 from collections import defaultdict
 from pathlib import Path
 from html import escape
+import math
 
 USERNAME = "hir-al-14"
 TOKEN = os.environ["GITHUB_TOKEN"]
